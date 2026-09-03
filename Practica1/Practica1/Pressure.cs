01ab4f91c95aafd4c1ebbd7ade7a3ec668b9401b
Nazar <you@example.com>
2026-09-03 23:16:17 +0700

    Первая практическая работа

---
+++ Practica1/Practica1/Pressure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    public class Pressure
+    {
+        public DateTime Date { get; set; }
+        public double Height { get; set; }
+        public int Value { get; set; }
+        public double Shirota { get; set; }
+        public double Dolgota { get; set; }
+
+        public Pressure() { }
+
+        public void FromStr(string f)
+        {
+            string[] values = f.Trim().Split(' ');
+            Date = DateTime.Parse(values[0]);
+            Height = double.Parse(values[1]);
+            Value = int.Parse(values[2]);
+            Shirota = double.Parse(values[3]);
+            Dolgota = double.Parse(values[4]);
+
+        }
+    }
+}
---
+++ Practica1/Practica1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Practica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            Pressure p1 = new Pressure();
+            p1.FromStr(s);
+            string[] result = { $"Дата: {p1.Date}, Высота: {p1.Height}, Значение: {p1.Value}, Широта: {p1.Shirota}, Долгота: {p1.Dolgota}" };
+            File.WriteAllLines("file.txt" +
+                "", result);
+
+        }
+    }
+}

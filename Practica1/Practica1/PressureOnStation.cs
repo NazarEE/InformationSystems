f1eee56c83982c7e9f0dc6485d2731015399cd37
Nazar <you@example.com>
2026-09-13 15:38:53 +0700

    Практика 2: добавлены 2 новых типпа объектов

---
+++ Practica1/Practica1/PressureOnStation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class PressureOnStation : Pressure
+    {
+        public string StationName { get; set; }
+        public bool CorrectData { get; set; }
+
+        public PressureOnStation() { }
+    }
+}
